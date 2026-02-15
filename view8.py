bb10c1d74c10b7912e7bd796f3b10e38871ce8d6
hasherezade <hasherezade@users.noreply.github.com>
2026-02-15 20:56:20 +0100

    [FEATURE] In view8_util: added new util functions. Included and excluded functions loaded from files

--- view8.py
+++ view8.py
@@ -2,10 +2,9 @@
 import argparse
 import os
 
-from view8_util import export_to_file
+from view8_util import *
 from Parser.parse_v8cache import parse_v8cache_file, parse_disassembled_file
-from Simplify.global_scope_replace import *
-
+from Simplify.global_scope_replace import replace_global_scope
 ####
 
 def disassemble(in_file, input_is_disassembled, disassembler):
@@ -20,36 +19,65 @@ def disassemble(in_file, input_is_disassembled, disassembler):
     
     return parse_disassembled_file(out_name)
 
-def decompile(all_functions, verbosity):
+def decompile(all_functions):
     # Decompile
     print(f"Decompiling {len(all_functions)} functions.")
     for name in list(all_functions)[::-1]:
         all_functions[name].decompile()
-    replace_global_scope(all_functions, verbosity)
+
+def propagate_global_scope(all_func, verbosity):
+    if replace_global_scope(all_func, verbosity):
+        if verbosity:
+            print("Replace global scope done.")
+        return True
+    return False
 
 ###
+
+def load_functions_set(filename):
+    try:
+        with open(filename, "r") as file:
+            deobf_funcs = set(line.strip() for line in file)
+            return deobf_funcs
+    except FileNotFoundError:
+        return None
+    return None
 
 def main():
     parser = argparse.ArgumentParser(description="View8: V8 cache decompiler.")
     group = parser.add_mutually_exclusive_group(required=False)
     group.add_argument('--input_format', '-f', choices=['raw', 'serialized', 'disassembled'],
-                        help="Specify the input format. Options are: 'raw', 'serialized', 'disassembled'(mutually exclusive)",
-                        default='raw')
+        help="Specify the input format. Options are: 'raw', 'serialized', 'disassembled'(mutually exclusive)", default='raw')
     parser.add_argument('--inp', '-i', help="The input file name.", default=None, required=True)
     parser.add_argument('--out', '-o', help="The output file name.", default=None)
-    parser.add_argument('--path', '-p', help="Path to disassembler binary.", default=None)
+    parser.add_argument('--path', '-p', help="Path to disassembler binary. Required if the input is in the raw format.", default=None)
     parser.add_argument('--export_format', '-e', nargs='+', choices=['v8_opcode', 'translated', 'decompiled', 'serialized'], 
                         help="Specify the export format(s). Options are 'v8_opcode', 'translated', and 'decompiled'. Multiple options can be combined.", 
                         default=['decompiled'])
+    parser.add_argument('--scope', help="Propagate scope arguments.", default=1, type=int, required=False)
     parser.add_argument('--tree', '-t', help="Show functions tree, starting from a given node. To start from the default main function, use 'start'", default=None)
     parser.add_argument('--mainlimit', '-l', help="In tree mode: a tree with depth above this limit will be treated as different module than main", type=int, default=1)
-    parser.add_argument('--include', '-n', nargs='+', help="Functions tree to Include.", default=[])
-    parser.add_argument('--exclude', '-x', nargs='+', help="Functions tree to Exclude.", default=[])
+    parser.add_argument('--include', '-n', help="Functions to Include (file containig a list)", default=None)
+    parser.add_argument('--exclude', '-x', help="Functions to Exclude (file containig a list)", default=None)
+    parser.add_argument('--func', help="A function to be displayed.", default=None, required=False)
+    parser.add_argument('--show_all', help="Should show lines marked as hidden (in function display mode)", default=False, required=False, action='store_true')
     parser.add_argument('--verbosity', '-v', help="Verbosity level (0-3)", default=0, type=int, required=False)
     args = parser.parse_args()
     
     if not os.path.isfile(args.inp):
         raise FileNotFoundError(f"The input file {args.inp} does not exist.")
+
+    funcs_to_include = None
+    if args.include:
+        funcs_to_include = load_functions_set(args.include)
+    if funcs_to_include:
+        print(f"Include: {len(funcs_to_include)} functions")
+
+    funcs_to_exclude = None
+    if args.exclude:
+        funcs_to_exclude = load_functions_set(args.exclude)
+    if funcs_to_exclude:
+        print(f"Exclude: {len(funcs_to_exclude)} functions")
 
     if ('serialized' in args.input_format):
         print(f"Reading from serialized, already decompiled input: {args.inp}")
@@ -59,7 +87,24 @@ def main():
         if 'disassembled' in args.input_format:
             disassembled = True
         all_func = disassemble(args.inp, disassembled, args.path)
-        decompile(all_func, args.verbosity)
+        decompile(all_func)
+
+    if args.scope:
+        print("Propagating scope arguments...")
+        propagate_global_scope(all_func, args.verbosity)
+
+    # print a single selected function:
+    if args.func:
+        func_name = args.func
+        filtered = find_functions_by_name(all_func, func_name)
+        if not func_name in filtered:
+            print(f"Function {func_name} was not found. Found {len(filtered)} similar names.")
+            for key in filtered.keys():
+                print(key)
+        if len(filtered) == 0:
+            return
+        print_funcs(filtered, args.show_all)
+        return
 
     if args.tree:
         tree_root = args.tree
@@ -67,12 +112,12 @@ def main():
             tree_root = get_start_function(all_func)
         items_map = split_trees(all_func, tree_root)
         if args.out:
-            save_trees(all_func, tree_root, args.mainlimit, items_map, args.out, args.export_format)
+            save_trees(all_func, tree_root, args.mainlimit, items_map, args.out, args.export_format, funcs_to_exclude)
             print(f"Done.")
             return
 
-    if args.out: 
-        export_to_file(args.out, all_func, args.export_format, args.include, args.exclude)
+    if args.out:
+        export_to_file(args.out, all_func, args.export_format, funcs_to_include, funcs_to_exclude)
     print(f"Done.")
 
 
--- view8_util.py
+++ view8_util.py
@@ -29,6 +29,132 @@ def get_all_children(functions, curr_func):
             children_list.append(func_name)
     return children_list
 
+def next_visible_line(func, indx, is_backward=False):
+    """
+    Next non-empty, visible line
+    Returns Index
+    """
+    if is_backward:
+        step = (-1)
+    else:
+        step = 1
+    indx += step
+    while (indx >= 0 and indx < len(func.code)):
+        if ((not func.code[indx].visible) or (not func.code[indx].decompiled) or (func.code[indx].decompiled.strip() == "")):
+            indx += step
+            continue
+        return indx
+    return None
+
+###
+
+def _rename_functions(
+    functions: dict[str, SharedFunctionInfo],
+    renamed_dict: dict[str, str],
+) -> int:
+    renamed_count = 0
+    new_functions: dict[str, SharedFunctionInfo] = {}
+
+    for name, func in functions.items():
+        if name in renamed_dict:
+            new_name = renamed_dict[name]
+            func.name = new_name
+            new_functions[new_name] = func
+            renamed_count += 1
+        else:
+            new_functions[name] = func
+
+    # mutate the original dict in place (if callers hold a reference)
+    functions.clear()
+    functions.update(new_functions)
+    return renamed_count
+
+def rename_functions_in_code(
+    functions: dict[str, SharedFunctionInfo],
+    renamed_dict: dict[str, str],
+    verbosity: int
+) -> int:
+
+    renamed = _rename_functions(functions, renamed_dict)
+    if verbosity:
+        print(f"Renamed functions: {renamed}")
+
+    func_pattern = r'\b(func_[a-zA-Z0-9_$]+_0x[0-9a-fA-F]+)\b'
+    regex = re.compile(func_pattern)
+    
+    for func in functions.values():
+        indx = 0
+        while True:
+            indx = next_visible_line(func, indx)
+            if indx is None:
+                break
+
+            line = func.code[indx].decompiled
+
+            # Replace only if the found name is in renamed_dict
+            def repl(m):
+                name = m.group(1)
+                return renamed_dict.get(name, name)
+
+            new_line = regex.sub(repl, line)
+
+            if new_line != line:
+                func.code[indx].decompiled = new_line
+                if verbosity:
+                    print(f"REPL: {line.strip()} -> {new_line.strip()}")
+    return renamed
+
+###
+
+def print_func(func_name, func, show_hidden=False, show_line_num=True, show_const=False, show_line_meta=False):
+    print("###")
+    print(f"# {func_name}")
+    print(f"# Declarer: {func.declarer}")
+    if func.metadata:
+        print(f"Metadata: {func.metadata}")
+    if show_const:
+        print(f"# Const Pool")
+        print(func.const_pool)
+    print(f"# Code")
+    indx = 0
+    i = 0
+    for i in range(len(func.code)):
+        line_obj = func.code[i]
+        if not line_obj.decompiled:
+            continue
+        if not show_hidden and not line_obj.visible:
+            continue
+        indx += 1
+        line = line_obj.decompiled
+
+        meta = ""
+        if show_line_meta:
+            if line_obj.metadata:
+                meta = f" # {line_obj.metadata}"
+
+        if show_line_num:
+            if indx != i:
+                print(f"{indx}|{i}|{line}{meta}")
+            else:
+                print(f"{indx}|{line}{meta}")
+        else:
+            print(f"{line}")
+#
+
+def print_funcs(functions, show_hidden=False, show_line_num=True, show_const=False, show_line_meta=False):
+    for func_name, func in functions.items():
+        print_func(func_name, func, show_hidden, show_line_num, show_const, show_line_meta)
+
+def find_functions_by_name(all_func, name):
+    funcs = dict()
+    if name in all_func:
+        funcs[name] = all_func[name]
+        return funcs
+    sub_name = "_" + name + "_"
+    for key in all_func.keys():
+        if sub_name in key:
+            funcs[key] = all_func[key]
+    return funcs
 ###
 
 def build_declaration_map(functions):
@@ -69,28 +195,47 @@ def get_included_functions(all_functions, include_list):
 
 def _export_to_file(out_name, all_functions, format_list, included_list = None, excluded_list = None):
     with open(out_name, "w") as f:
-        print(f"Exporting to file {out_name}.")
+        print(f"Exporting to file {out_name}")
         for function_name in list(all_functions)[::-1]:
             include = True
             if (excluded_list is not None and len(excluded_list)) and (function_name in excluded_list):
                 include = False
             if (included_list is not None and len(included_list)) and (function_name not in included_list):
                 include = False
-            if include:
-                f.write(all_functions[function_name].export(export_v8code="v8_opcode" in format_list, export_translated="translated" in format_list, export_decompiled="decompiled" in format_list))
+            if not all_functions[function_name].visible:
+                include = False
+            if not include:
+                continue
+            f.write(all_functions[function_name].export(export_v8code="v8_opcode" in format_list, export_translated="translated" in format_list, export_decompiled="decompiled" in format_list))
+
+def _get_extension(filename):
+    ext = None
+    if '.' in filename:
+        ext = '.' + filename.rsplit('.', 1)[-1]
+    return ext
+
+def _add_or_replace_extension(filename, new_ext):
+    if not new_ext.startswith('.'):
+        new_ext = '.' + new_ext
+    base = filename.rsplit('.', 1)[0] if '.' in filename else filename
+    return base + new_ext
 
 def export_to_file(out_name, all_functions, format_list, included_list = None, excluded_list = None):
     serialize_only = False
+    serialized_ext = ".pkl"
+    text_ext = ".txt"
     if ('serialized' in format_list):
-        serialized_name = out_name
         if len(format_list) == 1:
             serialize_only = True
-        else:
-            serialized_name += ".pkl"
+
+        serialized_name = _add_or_replace_extension(out_name, serialized_ext)
         print(f"Serializing to file: {serialized_name}")
         save_functions_to_file(all_functions, serialized_name)
+
         if serialize_only:
             return
+    if _get_extension(out_name) == serialized_ext:
+        out_name = _add_or_replace_extension(out_name, text_ext)
     _export_to_file(out_name, all_functions, format_list, included_list, excluded_list)
 
 ###
@@ -121,7 +266,7 @@ def create_dirs(nested_directory):
         print(f"An error occurred: {e}")
     return is_ok
 
-def save_trees(all_functions, main_func, main_limit, items_map, out_dir, export_format):
+def save_trees(all_functions, main_func, main_limit, items_map, out_dir, export_format, excluded_list):
     # export the root function and directly related:
     main_set = [main_func]
     for name, filtered_func in items_map.items():
@@ -142,4 +287,7 @@ def save_trees(all_functions, main_func, main_limit, items_map, out_dir, export_
         dirs = os.path.join(out_dir, subdir)
         create_dirs(dirs)
         out_path = os.path.join(out_dir, subdir, file_name)
-        export_to_file(out_path, all_functions, export_format, filtered_func)
+        export_to_file(out_path, all_functions, export_format, filtered_func, excluded_list)
+
+###
+
